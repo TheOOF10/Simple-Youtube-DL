b5981a113882d4e99e82596e3b94f25994dd6f9b
TheOOF10 <71931452+TheOOF10@users.noreply.github.com>
2024-03-31 22:56:03 +0300

    Update Simple-YT-DL.py
    Added 4K and 1440p support
    No attribute gives output instead of error
    Downloading in high resolutions such as 1080p downloads at 60fps when possible
    Optimizations mostly targeted at storage space
    Other minor changes

--- Simple-YT-DL.py
+++ Simple-YT-DL.py
@@ -2,14 +2,17 @@ print("Loading...")
 from pytube import YouTube
 import sys
 import os
-import ffmpeg
 from moviepy.editor import VideoFileClip
 from moviepy.editor import AudioFileClip
 arguments = sys.argv
 script_name = arguments[0]
-first = arguments[1]
-firstl = first[:8]
+try:
+    first = arguments[1]
+except IndexError:
+    print("ERROR: Argument not found type --help for help")
+    sys.exit(1) 
 
+firstl = first[:8]
 if first == "--help":
     print("Tutorial Downloading a video")
     print("type the command you just entered but instead of --help insert the url")
@@ -18,47 +21,76 @@ if first == "--help":
     print("There is nothing involving arguments and stuff just the instructions above it's called Simple Youtube DL for a reason")
 elif firstl == "https://" or firstl == "http://w" or firstl == "http://y" or firstl == "www.yout" or firstl == "youtube.": #check for url
     url = YouTube(first)
+    #for stream in url.streams:
+    #    print(stream)
     print("Do you want to download either video or audio")
     c = input()
     if c == "video" or c == "v":
         print("You selected video mode")
         print("What resolution do you want to download the video in? (type number)")
-        print("1. 1080p")
-        print("2. 720p")
-        print("3. 360p")
+        print("1. 4K")
+        print("2. 1440p")
+        print("3. 1080p")
+        print("4. 720p")
+        print("5. 360p")
         res = input()
         text = "Downloading " + url.title + "..."
         if int(res) == 1:
-            stream = url.streams.get_by_itag(137)
-            stream.download(filename="tmp.mp4")
-            stream = url.streams.get_by_itag(22)
-            stream.download(filename="tmpaudio.mp4")
+            stream = url.streams.get_by_itag(337)
+            try:
+                stream.download(filename="tmp.mp4")
+            except AttributeError:
+                print("ERROR could not download at 60fps trying at 30fps")
+                stream = url.streams.get_by_itag(313)
+                stream.download(filename="tmp.mp4")
+            stream = url.streams.get_by_itag(251)
+            stream.download(filename="tmpaudio.webm")
         elif int(res) == 2:
+            stream = url.streams.get_by_itag(336)
+            try:
+                stream.download(filename="tmp.mp4")
+            except AttributeError:
+                print("ERROR could not download at 60fps trying at 30fps")
+                stream = url.streams.get_by_itag(271)
+                stream.download(filename="tmp.mp4")
+            stream = url.streams.get_by_itag(251)
+            stream.download(filename="tmpaudio.webm")
+        elif int(res) == 3:
+            stream = url.streams.get_by_itag(335)
+            try:
+                stream.download(filename="tmp.mp4")
+            except AttributeError:
+                print("ERROR could not download at 60fps trying at 30fps")
+                stream = url.streams.get_by_itag(137)
+                stream.download(filename="tmp.mp4")
+            stream = url.streams.get_by_itag(251)
+            stream.download(filename="tmpaudio.webm")
+        elif int(res) == 4:
             stream = url.streams.get_by_itag(22)
             stream.download()
-        elif int(res) == 3:
+        elif int(res) == 5:
             stream = url.streams.get_by_itag(18)
             stream.download()
         else:
             print("ERROR wrong input")
-        if int(res) < 1 or int(res) == 1: #added < because there might be more resolutions in the future
+        if int(res) < 3 or int(res) == 3:
             print("Finalazing...")
             outfile = url.title + ".mp4"
             video_clip = VideoFileClip("tmp.mp4")
-            audio_clip = AudioFileClip("tmpaudio.mp4")
+            audio_clip = AudioFileClip("tmpaudio.webm")
             video_clip = video_clip.set_audio(audio_clip)
             video_clip.write_videofile(outfile)
     elif c == "audio" or c == "a":
         print("You selected audio mode")
         text = "Downloading " + url.title + "..."
         print(text)
-        stream = url.streams.get_by_itag(18)
+        stream = url.streams.get_by_itag(251)
         stream.download()
         print("Finalazing...")
-        filename = url.title + ".mp4"
+        filename = url.title + ".webm"
         finalname = url.title + ".mp3"
-        clip = VideoFileClip(filename)
-        clip.audio.write_audiofile(finalname)
+        clip = AudioFileClip(filename)
+        clip.write_audiofile(finalname)
         os.remove(filename)
         print("Done")
         print("Audio saved as " + finalname)
